fb5200024ad8d72699554d52f5f318e91a917a33
Gliderman <Nate@Gs-MacBook-Pro.local>
2015-09-03 11:12:40 -0400

    First 3 Java episodes
    Added code for the first 3 Java episodes.

---
+++ Java/Ep2 - System prints/Project1.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class Project1 {
+
+    public static void main(String[] args) {
+        int i = 9;
+        System.out.println("Hello, World #"+i+"!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ Java/Ep3 - Introducing primatives/Project1.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class Project1 {
+
+    static int intValue = 6;
+    static float floatValue = 3.14f;
+    static boolean booleanValue = true;
+    
+    public static void main(String[] args) {
+        System.out.println("An integer value: " + intValue);
+        System.out.println("A float value: " + floatValue);
+        System.out.println("A boolean value: " + booleanValue);
+    }
+
+}
